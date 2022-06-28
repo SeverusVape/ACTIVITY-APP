52d0ff04ca6f7108e1119101b936cbd90b79d86b
Sergei Koshelev <koshelevworkmail@gmail.com>
2022-06-28 16:25:07 -0400

    Leaflet API integration

--- script.js
+++ script.js
@@ -10,3 +10,51 @@ const inputDistance = document.querySelector(".form__input--distance");
 const inputDuration = document.querySelector(".form__input--duration");
 const inputCadence = document.querySelector(".form__input--cadence");
 const inputElevation = document.querySelector(".form__input--elevation");
+
+// * GEOLOCATION WITH LEAFLET
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+        function (position) {
+            const { longitude } = position.coords;
+            const { latitude } = position.coords;
+            const coords = [latitude, longitude];
+
+            // leafllet
+            const map = L.map("map").setView(coords, 13);
+            L.tileLayer(
+                "https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png",
+                {
+                    attribution:
+                        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+                }
+            ).addTo(map);
+
+            map.on("click", function (mapEvent) {
+                const { lat, lng } = mapEvent.latlng;
+                // custom pin
+                const myIcon = L.icon({
+                    iconUrl: "pic/dog.gif",
+                    iconSize: [90, 90],
+                    iconAnchor: [45, 34],
+                });
+
+                L.marker([lat, lng], { icon: myIcon })
+                    .addTo(map)
+                    .bindPopup(
+                        L.popup({
+                            maxWidth: 250,
+                            minWidth: 100,
+                            autoClose: false,
+                            closeOnClick: false,
+                            className: "running-popup",
+                        })
+                    )
+                    .setPopupContent("Some content")
+                    .openPopup();
+            });
+        },
+        function () {
+            console.log("NO position finded");
+        }
+    );
+}
